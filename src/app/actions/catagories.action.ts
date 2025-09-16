4e7a49755077b561284718aab0e209809f44c1ff
WAbosrea <WAbosrea@momrah.gov.sa>
2025-09-16 17:23:54 +0300

    fix error

--- src/app/actions/catagories.action.ts
+++ src/app/actions/catagories.action.ts
@@ -5,15 +5,13 @@ import axios from "axios";
 
 async function getCatagories() {
     try{
-        const response =await axios.get("https://ecommerce.routemisr.com/api/v1/catagories");
+        const response =await axios.get("https://ecommerce.routemisr.com/api/v1/categories");
         debugger
          return{
             data:response?.data.data,
             status:response.status,
             message:response?.data.message,
          }
-
-
     }catch(error:unknown)
     {
         if(axios.isAxiosError(error)){
